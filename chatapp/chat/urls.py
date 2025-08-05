cfefe9e879a5c9a60f18078003819f33159f4dfb
NAGA SASANK <rockingshooters7@gmail.com>
2025-08-05 16:55:16 +0530

    Add files via upload

---
+++ chatapp/chat/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Add this line
+    path('<str:room_name>/', views.room, name='room'),
+]
