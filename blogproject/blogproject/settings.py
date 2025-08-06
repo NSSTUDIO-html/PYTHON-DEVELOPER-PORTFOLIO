60ada84d548d789cc6dce1f05b546c1964db4c1a
NAGA SASANK <rockingshooters7@gmail.com>
2025-08-06 12:02:13 +0530

    Update settings.py

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&nlw^%1a1rkcepv+3v!b-uk%g^u!s+q031k6_czxrkc))=tqlx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
