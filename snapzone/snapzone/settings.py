15aec9a119ea9eede3ea8307854fb7c97b176c2a
NAGA SASANK <rockingshooters7@gmail.com>
2025-08-06 12:08:38 +0530

    Update settings.py

--- snapzone/snapzone/settings.py
+++ snapzone/snapzone/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-%as&)a4)1@c2(3=k#1x*x((oms6t++bc&h6rs+e3x%x$ibk7nh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,3 +130,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
