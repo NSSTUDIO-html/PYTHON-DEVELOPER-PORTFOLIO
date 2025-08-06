1099fb241bef0a661775cce24e7928f460ec3a1d
NAGA SASANK <rockingshooters7@gmail.com>
2025-08-06 12:05:14 +0530

    Update settings.py

--- jobboard/jobboard/settings.py
+++ jobboard/jobboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v%2p0^$-r1_1#7*d74$i+%-5v#q$i*khkb!v#$5s6xlm&@%=10
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
